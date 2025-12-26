c8945197a0aa563209e5efe2489eb16ddb258e85
diego <diegolinaresd10@gmail.com>
2025-12-26 16:57:17 -0500

    finally....the rockkkk

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  // Excluir funciones de Supabase del build
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/supabase/functions/**'],
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
